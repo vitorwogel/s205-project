37733f9076f58472398dd505a710749cd0f60fe5
Vitor Wogel <vitorandre26@gmail.com>
2024-06-05 19:02:26 -0300

    feat: new menu feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
+// Saudacao ao usuario
 user_name = document.querySelector('.saudacao')
 
 user_name.innerHTML += 'Vitor'
+
+// abertura do menu
+const menu = document.querySelector('#menu')
+
+function openMenu() {
+    menu.style.width = "40%"
+}
+
+// fechamento do menu
+function closeMenu() {
+    menu.style.width = "0"
+}
